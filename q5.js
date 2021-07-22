1a10950baa6a81157fbb65def946d1e8750a23b8
PAbishake <87352808+PAbishake@users.noreply.github.com>
2021-07-23 03:35:42 +0530

    Delete q5.js

--- q5.js
+++
@@ -1,9 +0,0 @@
-function obj(){
-  let person = {
-    firstName : "Nimal",
-    lastName : "Raj",
-    age : 15,
-    city : "Kandy"
-  };
-  document.getElementById("q5").innerHTML= "My name is " + person.firstName + " " + person.lastName + ". " + "i'm " + person.age + " years old. I live in " + person.city ;
-}
